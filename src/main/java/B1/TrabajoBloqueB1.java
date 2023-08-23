635be9b0f1eb0318987a0fb767371e515ece2296
Jorge Ivan Cruz Beltran <jorge.cruz@intranetcomfenalco.com>
2023-08-22 22:51:30 -0500

    tipos de datos

---
+++ src/main/java/B1/TrabajoBloqueB1.java
@@ -0,0 +1,28 @@
+package B1;
+
+public class TrabajoBloqueB1 {
+    public static void main(String[] args) {
+
+        //primitivo
+        int num1=9;
+        double num3=19.9; //
+        char inicial= 'a';
+        boolean esActiva= true;
+
+        //wrapper
+        short valor=32767;
+        Integer num2=9; // int
+        Double num10=19.9; // double
+        Boolean esFalso=true;
+        Character caraterw='T';
+        String text= "hola mundo"; //ALMACENA texto
+
+
+        System.out.println("Esto e sun int primitivo"+num1);
+        System.out.println("Esto e sun double primitivo"+num3);
+        System.out.println("Esto e sun char primitivo"+inicial);
+        System.out.println("Esto e sun boolean primitivo"+esActiva);
+        System.out.println("Esto es un integer wrapper"+num2);
+
+    }
+}
